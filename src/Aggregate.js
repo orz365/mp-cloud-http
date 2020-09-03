15bd018df521e1452823f819b25a65ef03be83a6
liuwei <orz365@163.com>
2020-09-03 11:57:18 +0800

    增加聚合方法

--- src/Aggregate.js
+++ src/Aggregate.js
@@ -27,6 +27,28 @@ class Aggregate extends Common {
     }
 
     /**
+     * 聚合阶段。将输入记录根据给定的条件和边界划分成不同的组，每组即一个 bucket。
+     * @param object
+     * @return {Aggregate}
+     */
+    bucket(object) {
+        object = JSON.stringify(object)
+        this.query += `.bucket(${object})`
+        return this
+    }
+
+    /**
+     * 聚合阶段。将输入记录根据给定的条件划分成不同的组，每组即一个 bucket。
+     * @param object
+     * @return {Aggregate}
+     */
+    bucketAuto(object) {
+        object = JSON.stringify(object)
+        this.query += `.bucketAuto(${object})`
+        return this
+    }
+
+    /**
      * 聚合阶段。添加新字段到输出的记录。
      * @param object
      * @return {Aggregate}
@@ -69,6 +91,72 @@ class Aggregate extends Common {
      */
     count(countName) {
         this.query += `.count("${countName}")`
+        return this
+    }
+
+    /**
+     * 聚合阶段。将记录按照离给定点从近到远输出。
+     * @param object
+     * @return {Aggregate}
+     */
+    geoNear(object) {
+        object = JSON.stringify(object)
+        this.query += `.geoNear(${object})`
+        return this
+    }
+
+    /**
+     * 聚合阶段。根据条件过滤文档，并且把符合条件的文档传递给下一个流水线阶段。
+     * @param object
+     * @return {Aggregate}
+     */
+    match(object) {
+        object = JSON.stringify(object)
+        this.query += `.match(${object})`
+        return this
+    }
+
+    /**
+     * 聚合阶段。把指定的字段传递给下一个流水线，指定的字段可以是某个已经存在的字段，也可以是计算出来的新字段。
+     * @param object
+     * @return {Aggregate}
+     */
+    project(object) {
+        object = JSON.stringify(object)
+        this.query += `.project(${object})`
+        return this
+    }
+
+    /**
+     * 聚合阶段。指定一个已有字段作为输出的根节点，也可以指定一个计算出的新字段作为根节点。
+     * @param object
+     * @return {Aggregate}
+     */
+    replaceRoot(object) {
+        object = JSON.stringify(object)
+        this.query += `.replaceRoot(${object})`
+        return this
+    }
+
+    /**
+     * 聚合阶段。随机从文档中选取指定数量的记录。
+     * @param object
+     * @return {Aggregate}
+     */
+    sample(object) {
+        object = JSON.stringify(object)
+        this.query += `.sample(${object})`
+        return this
+    }
+
+    /**
+     * 聚合阶段。使用指定的数组字段中的每个元素，对文档进行拆分。拆分后，文档会从一个变为一个或多个，分别对应数组的每个元素。
+     * @param object
+     * @return {Aggregate}
+     */
+    unwind(object) {
+        object = JSON.stringify(object)
+        this.query += `.unwind(${object})`
         return this
     }
 

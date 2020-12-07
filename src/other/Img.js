1c5bc3d6a65821bf449176b67cca85bf9d87a407
liuwei <orz365@163.com>
2020-12-07 09:14:37 +0800

    微信图片相关接口调用

--- src/other/Img.js
+++ src/other/Img.js
@@ -2,6 +2,7 @@ const logger = require('../utils/logger')
 const axios = require('axios')
 const {getToken} = require('../utils/token')
 const querystring = require('querystring')
+const FormData = require('form-data')
 
 class Img {
 
@@ -21,15 +22,40 @@ class Img {
     async aiCrop(img) {
         let access_token = await getToken(this.env, this.appid, this.appsecret, this.access_token)
 
-        return new Promise((resolve, reject) => {
-            axios.post(`https://api.weixin.qq.com/cv/img/aicrop?img_url=${img}&access_token=${access_token}`).then(res => {
-                logger.debug(res)
-                resolve(res)
-            }).catch(err => {
-                logger.error(err)
-                reject(err)
+        let promise = null
+        if (typeof img === 'string') {
+            promise = new Promise((resolve, reject) => {
+                axios.post(`https://api.weixin.qq.com/cv/img/aicrop?img_url=${img}&access_token=${access_token}`).then(res => {
+                    logger.debug(res)
+                    resolve(res)
+                }).catch(err => {
+                    logger.error(err)
+                    reject(err)
+                })
             })
-        })
+        } else {
+            let url = `https://api.weixin.qq.com/cv/img/aicrop?access_token=${access_token}`
+            let form = new FormData()
+            form.append('access_token', access_token)
+            form.append('img', img)
+            // Object.keys(img).forEach(key => {
+            //     form.append(key, img[key])
+            // })
+            promise = new Promise((resolve, reject) => {
+                let data = ''
+                form.submit(url, (err, res) => {
+                    res.on('data', (buffer) => {
+                        data += buffer
+                    })
+                    res.on('end', () => {
+                        logger.debug('[返回的数据]', data)
+                        resolve(JSON.parse(data))
+                    })
+
+                })
+            })
+        }
+        return promise;
     }
 
     /**
@@ -39,16 +65,50 @@ class Img {
      */
     async scanQRCode(img) {
         let access_token = await getToken(this.env, this.appid, this.appsecret, this.access_token)
+        let promise = null
 
-        return new Promise((resolve, reject) => {
-            axios.post(`https://api.weixin.qq.com/cv/img/qrcode?img_url=${img}&access_token=${access_token}`).then(res => {
-                logger.debug(res.data)
-                resolve(res)
-            }).catch(err => {
-                logger.error(err)
-                reject(err)
+        if (typeof img === 'string') {
+            promise = new Promise((resolve, reject) => {
+                axios.post(`https://api.weixin.qq.com/cv/img/qrcode?img_url=${img}&access_token=${access_token}`).then(res => {
+                    logger.debug(res.data)
+                    resolve(res)
+                }).catch(err => {
+                    logger.error(err)
+                    reject(err)
+                })
             })
-        })
+        } else {
+            let url = `https://api.weixin.qq.com/cv/img/qrcode?access_token=${access_token}`
+            let form = new FormData()
+            form.append('access_token', access_token)
+            form.append('img', img)
+            promise = new Promise((resolve, reject) => {
+                axios({
+                    method: 'post',
+                    url: url,
+                    data: form,
+                    headers: form.getHeaders()
+                }).then(function (res) {
+                    console.log(res)
+                }).catch(err => {
+                    console.error(err)
+                })
+
+                // let data = ''
+                // form.submit(url, (err, res) => {
+                //     res.on('data', (buffer) => {
+                //         data += buffer
+                //     })
+                //     res.on('end', () => {
+                //         logger.debug('[返回的数据]', data)
+                //         resolve(JSON.parse(data))
+                //     })
+                //
+                // })
+            })
+        }
+
+        return promise
     }
 
     /**

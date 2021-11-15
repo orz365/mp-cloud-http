a6e3f89d438ce33c147c618a91929f53f5cb0ae7
liuwei <orz365@163.com>
2021-11-15 09:09:47 +0800

    获取用户访问小程序日留存

--- src/other/Analysis.js
+++ src/other/Analysis.js
@@ -23,13 +23,39 @@ class Analysis {
         let access_token = await getToken(this.env, this.appid, this.appsecret, this.access_token)
 
         let data = {
-            access_token,
             begin_date,
             end_date
         }
 
         let promise = new Promise((resolve, reject) => {
-            let url = api.analysis.getDailySummary()
+            let url = api.analysis.getDailySummary(access_token)
+            axios.post(url,data).then(res => {
+                logger.debug(res.data)
+                resolve(res)
+            }).catch(err => {
+                logger.error(err)
+                reject(err)
+            })
+        })
+        return promise;
+    }
+
+    /**
+     * 获取用户访问小程序日留存
+     * @param begin_date 开始日期 yyyymmdd
+     * @param end_date 结束日期 yyyymmdd
+     * @return {Promise<unknown>}
+     */
+    async getDailyRetain(begin_date, end_date = begin_date) {
+        let access_token = await getToken(this.env, this.appid, this.appsecret, this.access_token)
+
+        let data = {
+            begin_date,
+            end_date
+        }
+
+        let promise = new Promise((resolve, reject) => {
+            let url = api.analysis.getDailyRetain(access_token)
             axios.post(url,data).then(res => {
                 logger.debug(res.data)
                 resolve(res)
--- src/utils/api.js
+++ src/utils/api.js
@@ -1,15 +1,26 @@
-
-
 const prefix = 'https://api.weixin.qq.com'
 
 const api = {
-    analysis:{
-        getDailySummary: ()=>`${prefix}/datacube/getweanalysisappiddailysummarytrend`
+    analysis: {
+        /**
+         * [POST] 获取用户访问小程序数据概况
+         * @param begin_date
+         * @param end_date
+         * @return {string}
+         */
+        getDailySummary: (access_token) => `${prefix}/datacube/getweanalysisappiddailysummarytrend?access_token=${access_token}`,
+        /**
+         * [POST] 获取用户访问小程序日留存
+         * @param begin_date
+         * @param end_date
+         * @return {string}
+         */
+        getDailyRetain: (access_token) => `${prefix}/datacube/getweanalysisappiddailyretaininfo?access_token=${access_token}`,
     },
-    img:{
-        aiCrop:(img,access_token)=>`${prefix}/cv/img/aicrop?img_url=${img}&access_token=${access_token}`,
-        scanQRCode: (img,access_token)=>`${prefix}/cv/img/qrcode?img_url=${img}&access_token=${access_token}`,
-        superresolution: (img,access_token)=>`${prefix}/cv/img/superresolution?img_url=${img}&access_token=${access_token}`
+    img: {
+        aiCrop: (img, access_token) => `${prefix}/cv/img/aicrop?img_url=${img}&access_token=${access_token}`,
+        scanQRCode: (img, access_token) => `${prefix}/cv/img/qrcode?img_url=${img}&access_token=${access_token}`,
+        superresolution: (img, access_token) => `${prefix}/cv/img/superresolution?img_url=${img}&access_token=${access_token}`,
     },
 
 }
